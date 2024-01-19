44d0ad3822c868125f1173f918dd731ddacb2c08
Dương Anh Kiệt <43516744+ffyyytt@users.noreply.github.com>
2024-01-19 03:54:52 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,9 +13,14 @@ int main()
 {
     static char msg[] = "123456:myname=ffyytt&uid=1001&lstcmd=1";
     static char msg1[] = "&download=secret.txt";
-    static char key[] = "ABCDEFGHIJKLMNOP";
-    char output[200];
+    std::string key_b64 = "QUJDREVGR0hJSktMTU5PUA==";
+    std::string key = base64().base64_decode(key_b64);
+    std::string iv_b64 = "UE9OTUxLSklIR0ZFRENCQQ==";
+    std::string iv = base64().base64_decode(iv_b64);
+    std::string ciphertext_b64 = "zme+4BwXPPDT/IY3N+0DwLXvcjxZoHyveVHsLdd9JuGkZ1bIj1/JXKjVlIcvBH3O";
+    std::string ciphertext = base64().base64_decode(ciphertext_b64);
     char newmsg[100];
+    char output[200];
     
     md5 md5_ctx = md5();
     uint8 md5sum[md5::DIGEST_SIZE];
@@ -24,7 +29,13 @@ int main()
     md5_ctx.final(md5sum);
     Hash::bytesToHex(md5sum, md5::DIGEST_SIZE, output);
     std::cout << "MD5: " << output << std::endl;
-
+    
+    md5_ctx.init();
+    md5_ctx.update((uint8*)key.c_str(), key.length());
+    md5_ctx.final(md5sum);
+    Hash::bytesToHex(md5sum, md5::DIGEST_SIZE, output);
+    std::cout << "MD5: " << output << std::endl;
+    
     sha1 sha1_ctx;
     uint8 sha1sum[sha1::DIGEST_SIZE];
     sha1_ctx.init();
@@ -32,7 +43,7 @@ int main()
     sha1_ctx.final(sha1sum);
     Hash::bytesToHex(sha1sum, sha1::DIGEST_SIZE, output);
     std::cout << "SHA1: " << output << std::endl;
-
+    
     sha256 h, hcopy; //sha512 h;
     uint8 hsum[h.DIGEST_SIZE];
     h.init();
@@ -41,7 +52,7 @@ int main()
     Hash::bytesToHex(hsum, h.getDigestSize(), output);
     std::cout << output << std::endl;
     //c7ac037d5e6cd43ab7ef47b7fb48f1e7e735f5b7f668178fb8b74c127ea0949f
-
+    
     // 123456 is password (unknown to the attacker)
     // myname=ffyytt&uid=1001&lstcmd=1&mac=c7ac037d5e6cd43ab7ef47b7fb48f1e7e735f5b7f668178fb8b74c127ea0949f is query 
     // where myname=ffyytt&uid=1001&lstcmd=1 is realquery from user
@@ -50,12 +61,12 @@ int main()
     // where download=secret.txt is in query
     // new_query = realquery + pad + extra_message
     // mac = sha256(new_query) = hashLengthExtensionAttack (without key)
-
+    
     uint8 outputLEA[h.DIGEST_SIZE];
     LengthExtensionAttack::hashLengthExtensionAttack(hcopy, hsum, (unsigned char*)msg1, strlen(msg1), outputLEA);
     Hash::bytesToHex(outputLEA, h.getDigestSize(), output);
     std::cout << output << std::endl;
-
+    
     // Check
     strcpy(newmsg, msg);
     uint64 len = LengthExtensionAttack::padding((uint8*)newmsg, strlen(msg), h.getBlockSize());
@@ -64,32 +75,31 @@ int main()
     h.init();
     h.update((uint8*)newmsg, len);
     //h.update((uint8*)msg1, strlen(msg1));
-
+    
     h.final(hsum);
     Hash::bytesToHex(hsum, h.DIGEST_SIZE, output);
     std::cout << output << std::endl;
-
+    
     AES aes(AESKeyLength::AES_128);
     uint8* msgpad = AES::pkcs7_padding((uint8*)msg, strlen(msg));
-    uint8* aesoutput = aes.EncryptECB((uint8*)msgpad, AES::pkcs7_padding_length(strlen(msg)), (uint8*)key);
+    uint8* aesoutput = aes.EncryptCBC((uint8*)msgpad, AES::pkcs7_padding_length(strlen(msg)), (uint8*)key.c_str(), (uint8*)iv.c_str());
     std::cout << base64().base64_encode(aesoutput, AES::pkcs7_padding_length(strlen(msg))) << std::endl;
-    std::cout << base64().base64_encode((uint8*)msg, strlen(msg)) << std::endl;
-
+    uint8* decrypted = AES::pkcs7_unpad(aes.DecryptCBC((uint8*)ciphertext.c_str(), ciphertext.length(), (uint8*)key.c_str(), (uint8*)iv.c_str()), ciphertext.length());
+    std::cout << decrypted << std::endl;
     
     AES squareAES(AESKeyLength::SQUARE);
     squareAES.generatePlainTextForSquareAttack("plaintexts.txt");
-    squareAES.EncryptListInFile("plaintexts.txt", "ciphertexts.txt", 16 * 256, (uint8*)key);
+    squareAES.EncryptListInFile("plaintexts.txt", "ciphertexts.txt", 16 * 256, (uint8*)key.c_str());
     unsigned char squarekey[] = "AAAAAAAAAAAAAAAA";
     AES::squareAttack("ciphertexts.txt", squarekey);
     std::cout << squarekey << std::endl;
-
+    
     std::string prefixfn("prefix.txt");
     std::string outfn1("out1.bin");
     std::string outfn2("out2.bin");
     std::string defaultIV("0123456789abcdeffedcba9876543210");
     md5collgen(prefixfn, outfn1, outfn2, defaultIV);
-
+    
     std::cout << crc::crc32buf(msg, strlen(msg));
-
     return 0;
 }
